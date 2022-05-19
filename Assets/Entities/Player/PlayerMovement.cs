47f10eefbe8e7b47c36b09dfe9f5ebdc1b18d235
Ciaran Wilkes <S0293212@nescot.ac.uk>
2022-05-19 12:17:47 +0100

    Some prefabs done

---
+++ Assets/Entities/Player/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 0.05f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0) * speed;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0) * speed;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, 1, 0) * speed;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, -1, 0) * speed;
+        }
+    }
+}
+
