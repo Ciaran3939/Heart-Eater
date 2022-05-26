6ceb8f244db438570ee30b06624bde92828a708e
Ciaran Wilkes <S0293212@nescot.ac.uk>
2022-05-26 12:16:50 +0100

    Undid everything then added it back so it will work better.
    it's better

--- Assets/Entities/Player/PlayerMovement.cs
+++ Assets/Entities/Player/PlayerMovement.cs
@@ -34,6 +34,12 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.position += new Vector3(0, -1, 0) * speed;
         }
+        
+        float PlayerX = Mathf.Clamp(transform.position.x, -71, 71);
+        transform.position = new Vector3(PlayerX, transform.position.y, transform.position.z);
+        float PlayerY = Mathf.Clamp(transform.position.x, -54, 54);
+        transform.position = new Vector3(transform.position.x, PlayerY, transform.position.z);
+        //-54, 54
+
     }
 }
-
